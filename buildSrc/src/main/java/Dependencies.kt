2f10df75e9f9cc98383f9d578cb0acfdd295ebda
lenblazy <lenblazy@gmail.com>
2023-08-10 14:42:15 +0300

    Update all build.gradle files

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,31 +1,32 @@
+object Versions {
+    const val core = "1.9.0"
+    const val appcompat = "1.6.1"
+    const val materialUI = "1.9.0"
+    const val constraintlayout = "2.1.4"
 
-object Versions{
-    const val core="1.9.0"
-    const val appcompat="1.6.1"
-    const val materialUI="1.9.0"
-    const val constraintlayout="2.1.4"
+    const val testImplJunit = "4.13.2"
 
-    const val testImplJunit="4.13.2"
-
-    const val androidTestImplJunit="1.1.5"
-    const val androidTestImplEspresso="3.5.1"
+    const val androidTestImplJunit = "1.1.5"
+    const val androidTestImplEspresso = "3.5.1"
 
 }
 
-object Deps {
-    const val core="androidx.core:core-ktx:${Versions.core}"
-    const val appcompat="androidx.appcompat:appcompat:${Versions.appcompat}"
-    const val materialUI="com.google.android.material:material:${Versions.materialUI}"
-    const val constraintlayout="androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
+object Dep {
+    const val core = "androidx.core:core-ktx:${Versions.core}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val materialUI = "com.google.android.material:material:${Versions.materialUI}"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
 }
 
-object TestImplementation{
-    const val testImplJunit="junit:junit:${Versions.testImplJunit}"
+object TestImplDep {
+    const val testImplJunit = "junit:junit:${Versions.testImplJunit}"
 }
 
-object AndroidTestImplementation{
+object AndroidTestImplDep {
 
-    const val androidTestImplJunit="androidx.test.ext:junit:${Versions.androidTestImplJunit}"
-    const val androidTestImplEspresso="androidx.test.espresso:espresso-core:${Versions.androidTestImplEspresso}"
+    const val androidTestImplJunit = "androidx.test.ext:junit:${Versions.androidTestImplJunit}"
+    const val androidTestImplEspresso =
+        "androidx.test.espresso:espresso-core:${Versions.androidTestImplEspresso}"
 
 }
