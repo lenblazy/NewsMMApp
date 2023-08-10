24a15722beb05bd2b5d8e3de8f6906d5b0777416
lenblazy <lenblazy@gmail.com>
2023-08-10 17:33:36 +0300

    Inject usecase

--- news/news_domain/src/main/java/com/lenibonje/news_domain/di/NewsDomainModule.kt
+++ news/news_domain/src/main/java/com/lenibonje/news_domain/di/NewsDomainModule.kt
@@ -1,6 +1,9 @@
 package com.lenibonje.news_domain.di
 
+import com.lenibonje.news_domain.repository.NewsRepository
+import com.lenibonje.news_domain.useCase.GetNewsArticleUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
@@ -8,6 +11,9 @@ import dagger.hilt.components.SingletonComponent
 @Module
 object NewsDomainModule {
 
-
+    @Provides
+    fun getNewsArticleUseCase(newsRepository: NewsRepository): GetNewsArticleUseCase {
+        return GetNewsArticleUseCase(newsRepository)
+    }
 
 }
