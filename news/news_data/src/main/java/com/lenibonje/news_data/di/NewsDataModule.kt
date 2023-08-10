d8becb55e06a4af913b7839547c9e5a943fd0bec
lenblazy <lenblazy@gmail.com>
2023-08-10 16:17:59 +0300

    Add hilt deps to news_data and news_domain modules

---
+++ news/news_data/src/main/java/com/lenibonje/news_data/di/NewsDataModule.kt
@@ -0,0 +1,13 @@
+package com.lenibonje.news_data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NewsDataModule {
+
+
+
+}
