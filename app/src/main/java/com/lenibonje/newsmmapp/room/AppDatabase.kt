fc1dd0eaa380ecbeb6d8c9501d3fb570a5542a66
lenblazy <lenblazy@gmail.com>
2023-08-11 16:12:08 +0300

    Add ap db

---
+++ app/src/main/java/com/lenibonje/newsmmapp/room/AppDatabase.kt
@@ -0,0 +1,23 @@
+package com.lenibonje.newsmmapp.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities =[], version = 1, exportSchema = false, )
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        fun getInstance(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context = context,
+                klass = AppDatabase::class.java,
+                name = "app_db"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
+}
